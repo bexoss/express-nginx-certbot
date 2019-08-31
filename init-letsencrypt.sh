e75ce4c70d103cf34ea9f75a9ab7f77ae9b6514a
unknown <bexoss@gmail.com>
2019-08-31 22:18:15 +0900

    initial commit

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(ec2-15-164-234-220.ap-northeast-2.compute.amazonaws.com www.ec2-15-164-234-220.ap-northeast-2.compute.amazonaws.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
