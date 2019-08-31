71dff8a345a3b75f7a399eb0510d92103af65fe4
unknown <bexoss@gmail.com>
2019-08-31 22:19:45 +0900

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(ec2-15-164-234-220.ap-northeast-2.compute.amazonaws.com www.ec2-15-164-234-220.ap-northeast-2.compute.amazonaws.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="bexoss@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
